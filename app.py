87be61df8f4604ea209139d66f336517f183bb25
Toysin <toysin1203@hotmail.com>
2023-11-01 15:43:35 +0900

    #6-endpoint 활성화
    BaseModel을 사용해서 application/json body를 파싱하도록 했다.

--- app.py
+++ app.py
@@ -1,8 +1,40 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from utils.script import Script
+
+import uuid
+
 
 app = FastAPI()
 
+class ReqDto(BaseModel):
+    group_key: uuid.UUID
+    original_language: str
+    target_language: str
+    scripts: List[Script]
 
-@app.get("/endpoint")
-def endpoint():
-    return {"s3SavedKey": "dummy key"}
+@app.post("/endpoint")
+def endpoint(dto: ReqDto):
+    print(f"s3 source voice group key: {dto.group_key}")
+    print(f"original language: {dto.original_language}")
+    print(f"target language: {dto.target_language}")
+
+    print("scripts:")
+    for script in dto.scripts:
+        print("----")
+        print(f"script: {script}")
+
+    ## TODO: Get source file from S3 by group_key
+
+    ## TODO: Translate scripts
+
+    ## TODO: Create TTS file from translated scripts
+
+    ## TODO: Apply VC model to TTS file
+
+    ## TODO: Upload TTS file to S3
+
+    ## TODO: Return s3_saved_key
+    s3_saved_key = uuid.uuid4()
+    return {"s3_saved_key": s3_saved_key}
